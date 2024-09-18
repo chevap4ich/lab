84d5bf7d50afa1b5585bdfd3e18e3d322e1e3d69
chevap4ich <netparolya1298@gmail.com>
2024-09-18 14:15:43 +0300

    18.09.24

--- try.cpp
+++ try.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hi world";
-    std::cout << "Hello Alex";
+    int size = 0;
+    int pr = 1;
+    cout << "size of array - ";
+    cin >> size;
+    cout << "enter the numbers" << endl;
+    int* d_arr = new int[size];
+    for(int i = 0; i < size; i++){
+        cin >> d_arr[i];
+        pr = pr*d_arr[i];
+    }
+    cout << pr << endl;
+    cout << d_arr;
+
+
 }
